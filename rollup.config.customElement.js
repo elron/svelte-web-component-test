a20c917daa99a44b258120e18e742f8d76aa31c3
Elron <elron100duck@gmail.com>
2023-06-24 03:21:44 +0300

    fix

--- rollup.config.customElement.js
+++ rollup.config.customElement.js
@@ -1,7 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-// import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import svelte from 'rollup-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
@@ -23,7 +22,6 @@ export default {
 	plugins: [
 		typescript({
 			tsconfig: './tsconfig.json',
-			compilerOptions: { lib: ['es5', 'es6', 'dom'], target: 'es5' }
 		}),
 		svelte({
 			preprocess: sveltePreprocess({
